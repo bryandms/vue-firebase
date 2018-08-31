899557ad475cc2a7c05e20a40fdd68d0c3cd9fbe
Bryan Morales <bryandms@hotmail.com>
2018-08-31 15:57:26 -0600

    Transitions

---
+++ 10-Transitions/js/main.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: 'main',
+    data: {
+        display: true,
+        message: {
+            transition: 'CSS transitions with Vue.js',
+            animation: 'CSS animations with Vue.js',
+            animationCustom: 'Custom CSS animations with Vue.js',
+            betweenElements: 'Transitions between elements with Vue.js'
+        }
+    }
+});
