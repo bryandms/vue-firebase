4388177b6547f6aed2edf713331f7000be053c00
Bryan Morales <bryandms@hotmail.com>
2018-10-13 16:44:11 -0600

    Scoped css

---
+++ 29-Scoped-css/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>List without styles</h3>
+        <ul>
+            <li>Read about scoped CSS</li>
+            <li>Implement scoped CSS</li>
+        </ul>
+        <Backpack></Backpack>
+    </div>
+</template>
+
+<script>
+    import Backpack from './components/Backpack.vue';
+    export default {
+        components: { Backpack }
+    }
+</script>
+
+<style>
+</style>
