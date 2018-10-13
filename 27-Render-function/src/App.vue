cb9aed6fde57c818aac8379f7e09e9e38f70fac8
Bryan Morales <bryandms@hotmail.com>
2018-10-12 18:06:49 -0600

    Render function

---
+++ 27-Render-function/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <node type="nav">Nav node</node>
+        <node type="aside">Aside node</node>
+        <node type="section">Section node</node>
+        <node type="footer">Footer node</node>
+    </div>
+</template>
+
+<script>
+    import node from './components/Node.vue';
+    export default {
+        components: { node }
+    }
+</script>
---
+++ 27-Render-function/src/components/Node.vue
@@ -0,0 +1,11 @@
+<script>
+    export default {
+        props: ['type'],
+        render(createElement) {
+            return createElement(
+                this.type,
+                this.$slots.default
+            )
+        }
+    };
+</script>
