0ed69a67108776f2de2146b002c822d4ad15845f
Bryan Morales <bryandms@hotmail.com>
2018-10-14 14:35:35 -0600

    CSS modules

---
+++ 30-CSS-modules/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p :class="$style.red">This should be red</p>
+        <p :class="$style.bold">This should be bold</p>
+        <p :class="[$style.red, $style.bold]">This should be red and bold</p>
+        <p :class="{ [$style.red]: isRed }">Am I red?</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isRed: false
+            }
+        }
+    };
+</script>
+
+<style module>
+    .red {
+        color: red;
+    }
+    .bold {
+        font-weight: bold;
+    }
+</style>
