93fd28f29722716e20d9e4ca0c26132daf518b2c
Bryan Morales <bryandms@hotmail.com>
2018-10-14 16:26:30 -0600

    Vuex central store

---
+++ 32-Vuex-central-store/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <count></count>
+</template>
+
+<script>
+    import count from './components/Count.vue';
+    export default {
+        components: { count }
+    }
+</script>
+
+<style>
+</style>
---
+++ 32-Vuex-central-store/src/components/Count.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <h1>Count: {{ count }}</h1>
+        <button @click="increase">+</button>
+        <button @click="decrease">-</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            increase() {
+                // It should not be mutated in this way
+                this.$store.state.count++;      
+            },
+            decrease() {
+                // It should not be mutated in this way
+                this.$store.state.count--;      
+            }
+        },
+        computed: {
+            count() {
+                return this.$store.state.count;
+            }
+        }
+    }
+</script>
+
+<style>
+    body {
+        background-color: #222;
+        color: #DDD;
+    }
+    button {
+        border-radius: 50%;
+        border: 1px solid #DDD;
+        background: transparent;
+        color: #DDD;
+        width: 30px;
+        height: 30px;
+        margin: 0 1em;
+    }
+    .container {
+        margin: 2em;
+        border: 1px solid #DDD;
+        padding: 2em;
+        text-align: center;
+    }
+</style>
