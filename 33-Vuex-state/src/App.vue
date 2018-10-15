5fcf5e2c691af2f6de40f6094fca5bc5dd5689c8
Bryan Morales <bryandms@hotmail.com>
2018-10-15 08:13:07 -0600

    Vuex state

---
+++ 33-Vuex-state/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>Hi, I'm {{ name }} {{ lastname }} and I live in {{ country }}</p>
+</template>
+
+<script>
+    import { mapState } from 'vuex';
+
+    export default {
+        // computed: {
+        //     name() {
+        //         return this.$store.state.name;
+        //     }
+        // }
+        // computed: mapState({
+        //     name: state => state.name,
+        //     lastname: state => state.lastname
+        // })
+        computed: mapState(['name', 'lastname', 'country'])
+    }
+</script>
+
+<style>
+</style>
