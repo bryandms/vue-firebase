aeb6013f504769e0859cd99d5e52f7d6229db838
Bryan Morales <bryandms@hotmail.com>
2018-10-16 12:27:16 -0600

    Vuex mutations

---
+++ 35-Vuex-mutations/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <counter></counter>
+    </div>
+</template>
+
+<script>
+    import counter from './components/Counter.vue';
+
+    export default {
+        components: {
+            counter
+        }
+    }
+</script>
---
+++ 35-Vuex-mutations/src/components/Counter.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="container">
+        <h3>Counter: {{ count }}</h3>
+        <button @click="decrease">-</button>
+        <button @click="increase">+</button>
+    </section>
+</template>
+
+<script>
+    import { mapState, mapMutations } from 'vuex';
+
+    export default {
+        computed: mapState(['count']),
+        methods: mapMutations(['increase', 'decrease'])
+        // methods: {
+        //     increase() {
+        //         //this.$store.state.count++;
+        //         this.$store.commit('increase');
+        //     },
+        //     decrease() {
+        //         //this.$store.state.count--;
+        //         this.$store.commit('decrease');
+        //     }
+        // }
+    }
+</script>
+
+<style>
+    body {
+        background-color: #222;
+        color: #DDD;
+    }
+    button {
+        border-radius: 50%;
+        border: 1px solid #DDD;
+        background: transparent;
+        color: #DDD;
+        width: 30px;
+        height: 30px;
+        margin: 0 1em;
+    }
+    .container {
+        margin: 2em;
+        border: 1px solid #DDD;
+        padding: 2em;
+        text-align: center;
+    }
+</style>
