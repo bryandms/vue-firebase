d6afa747b9c45b806293467823c55794181091a1
Bryan Morales <bryandms@hotmail.com>
2018-10-20 10:33:23 -0600

    Vue router components

---
+++ 41-Vue-router-components/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <section>
+        <router-link to="/">Home</router-link>
+        <router-link to="/contact">Contact</router-link>
+        <router-view></router-view>
+    </section>
+</template>
