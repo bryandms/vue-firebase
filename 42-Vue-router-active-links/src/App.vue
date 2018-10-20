14bb975b2127be9ab28c6bab059adbaab1d7cd98
Bryan Morales <bryandms@hotmail.com>
2018-10-20 11:22:57 -0600

    Vue router active links

---
+++ 42-Vue-router-active-links/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <router-link to="/" active-class="active" tag="li" exact>
+            <a>Home</a>
+        </router-link>
+        <router-link to="/contact" active-class="active" tag="li">
+            <a>Contact</a>
+        </router-link>
+        <router-view></router-view>
+    </section>
+</template>
+
+<style>
+    .active a {
+        color: lightseagreen;
+    }
+</style>
