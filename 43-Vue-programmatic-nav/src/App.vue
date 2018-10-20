3cb1946bf8df9bdd90466463d152cf6439dea7b0
Bryan Morales <bryandms@hotmail.com>
2018-10-20 14:53:58 -0600

    Vue programmatic nav

---
+++ 43-Vue-programmatic-nav/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <router-link to="/" active-class="active" tag="li" exact>
+            <a>Home</a>
+        </router-link>
+        <router-link to="/contact" active-class="active" tag="li">
+            <a>Contact</a>
+        </router-link>
+        <hr>
+        <router-view></router-view>
+    </section>
+</template>
+
+<style>
+    .active a {
+        color: lightseagreen;
+    }
+</style>
---
+++ 43-Vue-programmatic-nav/src/components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <h3>Contact</h3>
+        <p>If you do not press the button you will be redirected in 5 seconds</p>
+        <button @click="goHome()">Go Home</button>
+    </section>
+</template>
+
+<script>
+    export default {
+        created() {
+            setTimeout(() => this.$router.push('/'), 5000)
+        },
+        methods: {
+            goHome() {
+                this.$router.push('/', () => console.log('onComplete'))
+            }
+        }
+    }
+</script>
