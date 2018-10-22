7a28ff417126b0a658ccd7147410c9d77d2f96e7
Bryan Morales <bryandms@hotmail.com>
2018-10-22 14:58:49 -0600

    Vue router named routes

---
+++ 48-Vue-router-named-routes/src/components/User.vue
@@ -0,0 +1,9 @@
+<template>
+    <section>
+        <h1>ID: {{ $route.params.id }}</h1>
+        <h2>Peter Sandoval</h2>
+        <router-link :to="{ name:'photos', params: { id: $route.params.id } }">Photos</router-link>
+        <router-link :to="{ name:'bio', params: { id: $route.params.id } }">Bio</router-link>
+        <router-view></router-view>
+    </section>
+</template>
---
+++ 48-Vue-router-named-routes/src/components/UserPhotos.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        <h5>Photos</h5>
+        <img src="https://image.flaticon.com/icons/svg/21/21104.svg" alt="User">
+    </section>
+</template>
