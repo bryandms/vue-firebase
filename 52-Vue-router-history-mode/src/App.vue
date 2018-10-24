c5f3b8c2e2774770b5c800aaf664e06bd3262b9d
Bryan Morales <bryandms@hotmail.com>
2018-10-24 08:44:28 -0600

    Vue router history mode

---
+++ 52-Vue-router-history-mode/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <main>
+        <router-link :to="{ name: 'home' }">Home</router-link>
+        <router-link :to="{ name: 'contact' }">contact</router-link>
+        <router-view></router-view>
+    </main>
+</template>
